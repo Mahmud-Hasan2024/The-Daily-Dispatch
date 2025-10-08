7520e80000219d5a13400a3cf8f45e9dd001bf08
Mahmud Hasan <mh.kid234@gmail.com>
2025-10-08 14:24:32 +0600

    added requirements.txt

--- daily_dispatch/settings.py
+++ daily_dispatch/settings.py
@@ -26,6 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
+
 ALLOWED_HOSTS = []
 
 
@@ -140,8 +143,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "http://localhost:8000"]
 
 
 # Email configuration
