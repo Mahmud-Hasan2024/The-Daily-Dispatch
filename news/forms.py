89e54fe0a0405323eb098a30df510ccba5111bfd
Mahmud Hasan <mh.kid234@gmail.com>
2025-10-10 13:22:34 +0600

    disabled texarea field resize

--- news/forms.py
+++ news/forms.py
@@ -5,13 +5,28 @@ class AdminArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ["title", "content", "image", "category", "status"]
+        widgets = {
+            "content": forms.Textarea(attrs={
+                "class": "textarea w-full h-32 resize-none"
+            }),
+        }
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ["title", "content", "image", "category"]
+        widgets = {
+            "content": forms.Textarea(attrs={
+                "class": "textarea w-full h-32 resize-none"
+            }),
+        }
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ["name", "description"]
+        widgets = {
+            "description": forms.Textarea(attrs={
+                "class": "textarea w-full h-24 resize-none"
+            }),
+        }
